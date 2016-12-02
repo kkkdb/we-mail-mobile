b2cc651b8efd3aefd6bb736407699b2ae2931620
dbkong <dbkong@leqee.com>
2016-12-02 16:59:33 +0800

    select ok

--- src/js/common.js
+++ src/js/common.js
@@ -8,3 +8,26 @@ function deepClone(obj){
     }
     return temp;
 }
+
+//获取json元素数量
+function getJsonLength(obj){
+    var size = 0, key;
+    for (key in obj) {
+    if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+}
+
+function objChangeArr (obj, needKey) {
+    var arr = [];
+    for(var key in obj){
+        if (needKey) {
+            var temp_obj = {};
+            temp_obj[key] = obj[key];
+            arr.push(temp_obj)
+        }else{
+            arr.push(obj[key])
+        }
+    }
+    return arr
+}
