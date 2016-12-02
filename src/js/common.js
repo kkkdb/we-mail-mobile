76f25e9609fdd8a85ca8db1193820508c04973a0
dbkong <dbkong@leqee.com>
2016-12-02 14:31:34 +0800

    commit first

---
+++ src/js/common.js
@@ -0,0 +1,10 @@
+function deepClone(obj){
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+    var temp = obj.constructor();
+    for (var key in obj) {
+        temp[key] = deepClone(obj[key]);
+    }
+    return temp;
+}
